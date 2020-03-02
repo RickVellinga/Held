8b49d863842a03136a771cf326a98091ce3cd875
RickVellinga <55230960+RickVellinga@users.noreply.github.com>
2020-03-02 14:42:57 +0100

    Add files via upload

---
+++ helden.js
@@ -0,0 +1,23 @@
+$('#barack img').click( function() {
+  $('#barack h3').slideToggle(700);
+  $('#barack p').slideToggle(700);
+
+})
+
+$('#robert img').click( function() {
+  $('#robert h3').slideToggle(700);
+  $('#robert p').slideToggle(700);
+
+})
+
+$('#thomas img').click( function() {
+  $('#thomas h3').slideToggle(700);
+  $('#thomas p').slideToggle(700);
+
+})
+
+$('#martin img').click( function() {
+  $('#martin h3').slideToggle(700);
+  $('#martin p').slideToggle(700);
+
+})
